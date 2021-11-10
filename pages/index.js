6a0d655b2acaf825b652982db6dd06f39e405dff
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2021-11-10 08:56:25 -0300

    Product rendering added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Grid,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <Layout>
-      <div>Productdd</div>
+      <h1>Products</h1>
+      <Grid container spacing={3}>
+        {data.products.map((product) => {
+          console.log(product.name);
+
+          return (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  />
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>R${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </Layout>
   );
 }
---
+++ utils/data.js
@@ -0,0 +1,66 @@
+const data = {
+  products: [
+    {
+      name: "ANTHEM 29",
+      category: "Mountain Bikes",
+      image: "/images/bike1.jpg",
+      price: 2100,
+      brand: "Giant",
+      rating: 4.6,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        "With an ALUXX SL aluminum frameset that’s engineered with 90mm of rear suspension travel and 100mm up front, this smooth-riding 29er is a master at conquering technical XC terrain.",
+    },
+    {
+      name: "ANTHEM 29",
+      category: "Mountain Bikes",
+      image: "/images/bike1.jpg",
+      price: 2100,
+      brand: "Giant",
+      rating: 4.6,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        "With an ALUXX SL aluminum frameset that’s engineered with 90mm of rear suspension travel and 100mm up front, this smooth-riding 29er is a master at conquering technical XC terrain.",
+    },
+    {
+      name: "ANTHEM 29",
+      category: "Mountain Bikes",
+      image: "/images/bike1.jpg",
+      price: 2100,
+      brand: "Giant",
+      rating: 4.6,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        "With an ALUXX SL aluminum frameset that’s engineered with 90mm of rear suspension travel and 100mm up front, this smooth-riding 29er is a master at conquering technical XC terrain.",
+    },
+    {
+      name: "ANTHEM 29",
+      category: "Mountain Bikes",
+      image: "/images/bike1.jpg",
+      price: 2100,
+      brand: "Giant",
+      rating: 4.6,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        "With an ALUXX SL aluminum frameset that’s engineered with 90mm of rear suspension travel and 100mm up front, this smooth-riding 29er is a master at conquering technical XC terrain.",
+    },
+    {
+      name: "ANTHEM 29",
+      category: "Mountain Bikes",
+      image: "/images/bike1.jpg",
+      price: 2100,
+      brand: "Giant",
+      rating: 4.6,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        "With an ALUXX SL aluminum frameset that’s engineered with 90mm of rear suspension travel and 100mm up front, this smooth-riding 29er is a master at conquering technical XC terrain.",
+    },
+  ],
+};
+
+export default data;
