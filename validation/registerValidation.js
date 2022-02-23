209dd649df3bec43e91070b77dd05bd17519a0f2
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-23 11:13:10 -0400

    feat(register): add validation schema

---
+++ validation/registerValidation.js
@@ -0,0 +1,25 @@
+import * as yup from "yup";
+
+const registerSchema = yup.object().shape({
+  name: yup.string().required("Enter a name"),
+  email: yup
+    .string()
+    .email("Enter a valid email address")
+    .min(2, "Name too short")
+    .required("Enter an email"),
+  password: yup
+    .string()
+    .min(6, "Password too short")
+    .matches(
+      /[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/,
+      "Your password needs at least one special character"
+    )
+    .matches(/[0-9]/, "Your password needs at least a number")
+    .required("Enter a valid password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords don't match")
+    .required("Confirm your password"),
+});
+
+export default registerSchema;
