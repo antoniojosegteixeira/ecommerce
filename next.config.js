7b13209a57288c82a1d91414353caecdba243792
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-23 10:54:57 -0400

    chore: add next-transpile-modules to next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-module.exports = {
+// next.config.js
+const withTM = require("next-transpile-modules")(["@hookform/resolvers"]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
-}
+});
