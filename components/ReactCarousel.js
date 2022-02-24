07b81feaed1f9d4001e7f09a34431c2ad598d6c3
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-24 16:05:24 -0400

    refactor: use swr to fetch top products data

--- components/ReactCarousel.js
+++ components/ReactCarousel.js
@@ -1,67 +1,50 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
+import useSWR from "swr";
 import {
   NoSsr,
   Grid,
   Box,
-  Container,
   List,
   ListItem,
   Typography,
 } from "@material-ui/core";
-import { AppContext } from "../utils/AppContext";
-import axios from "axios";
 import NextLink from "next/link";
 import useStyles from "../utils/styles";
 
 export default function ReactCarousel() {
   const classes = useStyles();
-  const { state, dispatch } = useContext(AppContext);
-  const { topProducts } = state;
+  const { data } = useSWR("/api/products/top-products");
 
-  useEffect(() => {
-    const getTopProducts = async () => {
-      const { data } = await axios.get(`/api/products/top-products`);
-      if (data) {
-        dispatch({ type: "ADD_TOP_PRODUCTS", payload: data });
-      }
-    };
-    getTopProducts();
-  }, []);
-
-  if (topProducts.length > 0) {
-    return (
-      <NoSsr>
-        <Grid container spacing={3} justifyContent="center">
-          {topProducts.length > 0 &&
-            topProducts.map((item) => {
-              return (
-                <Grid md={4} item key={item.name} style={{ maxWidth: "430px" }}>
-                  <NextLink href={`/product/${item.slug}`}>
-                    <Box className={classes.topProductCard}>
-                      <List>
-                        <ListItem>
-                          <img src={item.image} style={{ width: "100%" }} />
-                        </ListItem>
-                        <ListItem>
-                          <Typography
-                            component="span"
-                            variant="h5"
-                            align="center"
-                            style={{ width: "100%" }}
-                          >
-                            {item.name}
-                          </Typography>
-                        </ListItem>
-                      </List>
-                    </Box>
-                  </NextLink>
-                </Grid>
-              );
-            })}
-        </Grid>
-      </NoSsr>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <NoSsr>
+      <Grid container spacing={3} justifyContent="center">
+        {data &&
+          data.map((item) => {
+            return (
+              <Grid md={4} item key={item.name} style={{ maxWidth: "430px" }}>
+                <NextLink href={`/product/${item.slug}`}>
+                  <Box className={classes.topProductCard}>
+                    <List>
+                      <ListItem>
+                        <img src={item.image} style={{ width: "100%" }} />
+                      </ListItem>
+                      <ListItem>
+                        <Typography
+                          component="span"
+                          variant="h5"
+                          align="center"
+                          style={{ width: "100%" }}
+                        >
+                          {item.name}
+                        </Typography>
+                      </ListItem>
+                    </List>
+                  </Box>
+                </NextLink>
+              </Grid>
+            );
+          })}
+      </Grid>
+    </NoSsr>
+  );
 }
