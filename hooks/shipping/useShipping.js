1b1df167812173089b37ab759e7e958c3dbc26fb
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-25 04:57:35 -0400

    feat: add useShipping hook

---
+++ hooks/shipping/useShipping.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { AppContext } from "../../utils/AppContext";
+import { getProduct } from "../../http/cartOperations";
+import { useSnackbar } from "notistack";
+
+export function useShipping() {
+  const { state, dispatch } = useContext(AppContext);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const addShippingAddress = async (shippingData) => {
+    dispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: shippingData,
+    });
+    Cookies.set("userAddress", JSON.stringify(shippingData));
+  };
+
+  return { addShippingAddress };
+}
