9d4bb018a1bd34a8e07e1683d194c41f8d596547
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-25 07:11:29 -0400

    refactor: move price calculator to helper function

--- helpers/cartHelpers.js
+++ helpers/cartHelpers.js
@@ -2,3 +2,18 @@ export const checkItemStock = (cartItems, product) => {
   const existItem = cartItems.find((e) => e._id === product._id);
   return existItem ? existItem.quantity + 1 : 1;
 };
+
+export const calculateTotalCartPrice = (cartItems) => {
+  const round = (num) => Math.round((num + Number.EPSILON) * 100) / 100;
+  const itemsPrice = round(
+    cartItems.reduce((a, c) => a + c.price * c.quantity, 0)
+  );
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
+  const totalPrice = round(itemsPrice + shippingPrice);
+
+  return {
+    itemsPrice,
+    shippingPrice,
+    totalPrice,
+  };
+};
